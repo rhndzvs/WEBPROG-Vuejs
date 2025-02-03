6c856189bf058a9961d7c752a01eed533fae4b0d
rhndzvs <rrballesteros2@student.apc.edu.ph>
2025-02-03 17:00:07 +0800

    add insert comment/visitor feedback functionality

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,14 @@
 <template>
   <h1>Food</h1>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-  <br>
-  <food-item/> <br>
-  <food-item2/> <br>
+  <country/> <br>
+  <food-item/>
+  <food-item/>
   <food-item/> <br>
   <personal-profile/> <br>
+  <comment-form/>
+  <comment/>
 </template>
 
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+<script></script>
 
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-
-<style>
-</style>
+<style></style>
